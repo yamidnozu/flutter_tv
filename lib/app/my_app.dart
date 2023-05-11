aac77a3ca7431161840fcec5b56a0f7a27102105
eynoguer <edilson.noguera@bancolombia.com.co>
2023-05-10 20:02:44 -0500

    Se valida si hay internet

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const  Scaffold(),
       initialRoute: Routes.splash,
       routes: appRoutes,
     );
--- lib/app/presentation/modules/splash/views/splash.dart
+++ lib/app/presentation/modules/splash/views/splash.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:tv/app/data/repositories_implementation/connectivity_repositories_impl.dart';
+import 'package:tv/app/domain/repositories/connectivity_repository.dart';
 
-class SplashView extends StatelessWidget {
+// Convertir de StatelessWidget a StatefulWidget
+// Init state y hacer comprobacion si hay internet
+// Notar que realmente no se esta haciendo arquitectura limpia, ConnectivityRepositoryImpl pertenece
+// a la capa de datos, y SplashView pertenece a la capa de presentación,
+// de la capa de presentacion no deberia depender de la capa de datos
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    final ConnectivityRepository connectivityRepository =
+        ConnectivityRepositoryImpl();
+
+    final bool hasInternet = await connectivityRepository.hasInternet;
+
+    if (hasInternet) {
+      // Navigator.pushReplacementNamed(context, Routes.home);
+      print("✅ Hay internet");
+    } else {
+      // Navigator.pushReplacementNamed(context, Routes.noInternet);
+      print("❌ No hay internet");
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Center(
+      child: SizedBox(
+        width: 80,
+        height: 80,
+        child: CircularProgressIndicator(),
+      ),
+    ));
   }
 }
