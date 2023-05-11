86667e013cc288ff1c9f92c9389d8cd817af6055
eynoguer <edilson.noguera@bancolombia.com.co>
2023-05-10 21:07:30 -0500

    Se crea el inherited widget Injector que permite hacer inyección de repositories y sus implementaciones

--- lib/app/presentation/modules/splash/views/splash.dart
+++ lib/app/presentation/modules/splash/views/splash.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:tv/app/data/repositories_implementation/connectivity_repositories_impl.dart';
 import 'package:tv/app/domain/repositories/connectivity_repository.dart';
+import 'package:tv/main.dart';
 
 // Convertir de StatelessWidget a StatefulWidget
 // Init state y hacer comprobacion si hay internet
 // Notar que realmente no se esta haciendo arquitectura limpia, ConnectivityRepositoryImpl pertenece
 // a la capa de datos, y SplashView pertenece a la capa de presentación,
 // de la capa de presentacion no deberia depender de la capa de datos
+
+// Parte 4:
+//  De la clase Injector  recuperar connectivityRepository
+// usar WidgetsBinding.instance.addPostFrameCallback((_) { ... }) para esperar a que se construya el widget
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -19,12 +23,14 @@ class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-    _init();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _init();
+    });
   }
 
   Future<void> _init() async {
     final ConnectivityRepository connectivityRepository =
-        ConnectivityRepositoryImpl();
+        Injector.of(context).connectivityRepository;
 
     final bool hasInternet = await connectivityRepository.hasInternet;
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:tv/app/data/repositories_implementation/authentication_repository_impl.dart';
+import 'package:tv/app/data/repositories_implementation/connectivity_repositories_impl.dart';
+import 'package:tv/app/domain/repositories/authentication_repository.dart';
+import 'package:tv/app/domain/repositories/connectivity_repository.dart';
 import 'package:tv/app/my_app.dart';
 
 void main() {
-  runApp(const MyApp(),);
+  runApp(
+    Injector(
+      child: const MyApp(),
+      authenticationRepository: AuthenticationRepositoryImpl(),
+      connectivityRepository: ConnectivityRepositoryImpl(),
+    ),
+  );
+}
+
+class Injector extends InheritedWidget {
+  const Injector({
+    super.key,
+    required super.child,
+    required this.connectivityRepository,
+    required this.authenticationRepository,
+  });
+
+  final ConnectivityRepository connectivityRepository;
+  final AuthenticationRepository authenticationRepository;
+
+  // Aqui solo se van a declarar los repositorios y estos no van a cambiar durante toda la ejecucipon de la app
+  @override
+  bool updateShouldNotify(_) {
+    return false; // Por eso este false
+  }
+
+  static Injector of(BuildContext context) {
+    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
+    assert(injector != null, "No se encontro el injector");
+    return injector!;
+  }
 }
